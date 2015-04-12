24ea689c13f52a11f05ba58b79abdcd49faed5f0
Balaji S <balaaagi@gmail.com>
2015-04-12 22:54:41 +0530

    Initial Methods Implemented

--- src/me/balaaagi/cricinfocurler/CricInfo.java
+++ src/me/balaaagi/cricinfocurler/CricInfo.java
@@ -1,5 +1,167 @@
 package me.balaaagi.cricinfocurler;
 
-public class CricInfo {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
+import java.net.MalformedURLException;
+import java.net.Proxy;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Set;
 
+public class CricInfo {
+String match_URL=null;
+HashMap<String,String> overallScoreList=new HashMap<String,String>();
+HashMap<String,String> currentlyBatting=new HashMap<String,String>();
+
+String match_title=null;
+String matchCurrentScore=null;
+String matchCurrentOvers=null;
+boolean behindProxy=false;
+String proxyIPAdrress=null;
+String proxyPort=null;
+URL request_url=null;
+Proxy proxy=null;
+HttpURLConnection http_conn=null;
+
+public String getProxyIPAdrress() {
+	return proxyIPAdrress;
+}
+
+public void setProxyIPAdrress(String proxyIPAdrress) {
+	this.proxyIPAdrress = proxyIPAdrress;
+}
+
+public String getProxyPort() {
+	return proxyPort;
+}
+
+public void setProxyPort(String proxyPort) {
+	this.proxyPort = proxyPort;
+}
+
+
+
+public boolean isBehindProxy() {
+	return behindProxy;
+}
+
+public void setBehindProxy(boolean behindProxy) {
+	this.behindProxy = behindProxy;
+}
+public HashMap<String, String> getCurrentlyBatting() {
+	return currentlyBatting;
+}
+
+public void printScoreList(){
+	
+	Set<String> keys=this.overallScoreList.keySet();
+	for(String key : keys){
+		if(this.currentlyBatting.containsKey(key))
+			System.out.println(key+"*"+"\t\t"+this.overallScoreList.get(key));
+		else
+			System.out.println(key+" "+"\t\t"+this.overallScoreList.get(key));
+	}
+}
+
+public void connectESPNCricInfo() throws Exception{
+	 this.request_url= new URL(this.match_URL);
+	 
+	if(this.isBehindProxy()){
+		this.proxy= new Proxy(Proxy.Type.HTTP, new InetSocketAddress(this.getProxyIPAdrress().toString(), Integer.parseInt(this.getProxyPort())));
+		this.http_conn=(HttpURLConnection)request_url.openConnection(this.proxy);
+	}else{
+		this.http_conn=(HttpURLConnection)request_url.openConnection();
+	}
+	this.http_conn.setFollowRedirects(true);
+
+	this.http_conn.setConnectTimeout(100000);
+	this.http_conn.setReadTimeout(100000);
+	this.http_conn.setInstanceFollowRedirects(true);
+
+}
+public void getScoreCardFromCricInfo() throws Exception{
+	BufferedReader in = new BufferedReader(new InputStreamReader(
+	                                    this.http_conn.getInputStream()));
+	        String inputLine;
+	        while ((inputLine = in.readLine()) != null) {
+
+	            if(inputLine.indexOf("<title>")>-1)
+	                break;
+	        }
+	            
+	        in.close();
+	        // System.out.println("Current Score....");
+	        // System.out.println("Length:"+inputLine.length());
+	        inputLine=inputLine.substring(8,inputLine.indexOf("</title>"));
+	        System.out.println("Live Score");
+	        System.out.println(inputLine.substring(0,inputLine.indexOf("(")));
+	        String mainscore=inputLine.substring(inputLine.indexOf("(")+1,inputLine.indexOf(")"));
+	        String[] scoresplit=mainscore.split(",");
+	        
+	                System.out.println("Overs: "+scoresplit[0]);
+	                System.out.println("Batsmen in Strike:"+scoresplit[1]);
+	                if(scoresplit.length>2)
+	                    System.out.println("Batsmen in Non Strike:"+scoresplit[2]);
+	                else
+	                    System.out.println("Batsmen OUT!!!!");
+	                System.out.println("------------");
+	                if(scoresplit.length>2)
+	                    System.out.println("Bowler: "+scoresplit[3]);
+	                else
+	                    System.out.println("Bowler: "+scoresplit[2]);
+	    
+}
+
+public String getMatchCurrentOvers() {
+	return matchCurrentOvers;
+}
+
+public void setMatchCurrentOvers(String matchCurrentOvers) {
+	this.matchCurrentOvers = matchCurrentOvers;
+}
+
+public String getMatchCurrentScore() {
+	return matchCurrentScore;
+}
+
+public void setMatchCurrentScore(String matchCurrentScore) {
+	this.matchCurrentScore = matchCurrentScore;
+}
+
+public String getMatch_title() {
+	return match_title;
+}
+
+public void setMatch_title(String match_title) {
+	this.match_title = match_title;
+}
+
+public HashMap<String, String> getOverallScoreList() {
+	return overallScoreList;
+}
+
+public void setOverallScoreList(HashMap<String, String> overallScoreList) {
+	this.overallScoreList = overallScoreList;
+}
+
+public void updateScoreList(String playerName,String playerScore){
+	this.overallScoreList.put(playerName, playerScore);
+}
+public String getMatch_URL() {
+	return match_URL;
+}
+
+public void setMatch_URL(String match_URL) {
+	this.match_URL = match_URL;
+}
+
+public void batsmanOut(String playerName,String playerScore){
+	this.updateScoreList(playerName, playerScore);
+	this.currentlyBatting.remove(playerName);
+}
+
+ 
+	
 }
